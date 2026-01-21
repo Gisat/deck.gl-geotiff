ee658b915ddfc59634693d898a9b6f8b9eb43e56
Mariana Kecová <marianakecova@Mariana--MacBook-Pro.local>
2026-01-21 16:05:17 +0100

    refactor: move layer files to src/layers/ with barrel exports

--- geoimage/src/index.ts
+++ geoimage/src/index.ts
@@ -1,11 +1,12 @@
-import CogBitmapLayer from './cogbitmaplayer/CogBitmapLayer.ts';
-import CogTerrainLayer from './cogterrainlayer/CogTerrainLayer.ts';
-import cogtiles from './cogtiles/cogtiles.ts';
-import GeoImage from './geoimage/geoimage.ts';
+// noinspection JSUnusedGlobalSymbols
 
-export default {
-  CogBitmapLayer,
-  CogTerrainLayer,
-  cogtiles,
-  GeoImage,
-};
+// Export specific named layers directly
+// eslint-disable-next-line import/extensions
+export { CogBitmapLayer, CogTerrainLayer } from './layers/index';
+
+// Re-export default exports from sub-modules as named exports
+// eslint-disable-next-line import/extensions
+export { default as cogtiles } from './cogtiles/cogtiles';
+
+// eslint-disable-next-line import/extensions
+export { default as GeoImage } from './geoimage/geoimage';
